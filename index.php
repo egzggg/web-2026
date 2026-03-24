5dc1277ca9b08dc1e98c65480e8425269c69b02b
Egor Shevchuk <egorshevchuk207@gmail.com>
2026-03-24 23:15:12 +0300

    v2

--- index.php
+++ index.php
@@ -1,48 +1,24 @@
 <?php
-$date = $_POST['birthdate'];
-list($year, $month, $day) = explode('-', $date);
-$year = (int)$year;
-$month = (int)$month;
-$day = (int)$day;
+$nums1 = $_POST['nums1'];
+$nums2 = $_POST['nums2'];
 
-$DATE_BIRTHD = $month * 100 + $day;
+function sumNum(int $digit) {
+    return $digit % 100 + ((int)($digit / 10) % 10) + ($digit % 10);
+}
 
-switch (true) {
-    
-    case ($DATE_BIRTHD >= 120 && $DATE_BIRTHD <= 218):
-        echo "Водолей";
-        break;
-        case ($DATE_BIRTHD >= 219 && $DATE_BIRTHD <= 320):
-        echo "Рыбы";
-        break;
-    case ($DATE_BIRTHD >= 321 && $DATE_BIRTHD <= 419):
-        echo "Овен";
-        break;
-    case ($DATE_BIRTHD >= 420 && $DATE_BIRTHD <= 520):
-        echo "Телец";
-        break;
-    case ($DATE_BIRTHD >= 521 && $DATE_BIRTHD <= 621):
-        echo "Близнецы";
-        break;
-    case ($DATE_BIRTHD >= 622 && $DATE_BIRTHD <= 722):
-        echo "Рак";
-        break;
-    case ($DATE_BIRTHD >= 723 && $DATE_BIRTHD <= 822):
-        echo "Лев";
-        break;
-    case ($DATE_BIRTHD >= 823 && $DATE_BIRTHD <= 922):
-        echo "Дева";
-        break;
-    case ($DATE_BIRTHD >= 923 && $DATE_BIRTHD <= 1023):
-        echo "Весы";
-        break;
-    case ($DATE_BIRTHD >= 1024 && $DATE_BIRTHD <= 1122):
-        echo "Скорпион";
-        break;
-    case ($DATE_BIRTHD >= 1123 && $DATE_BIRTHD <= 1221):
-        echo "Стрелец";
-        break;
-    default:
-        echo "Козерог";
+function luckyNums(int $digit) {
+    $firstPart = sumNum((int)($digit / 1000));
+    $lastPart  = sumNum($digit % 1000);
+    if ($firstPart == $lastPart) {
+        echo $digit . "<br>";
+    }
+}
+
+if ($nums1 >= 100000 and $nums1 <= 999999 and $nums2 >= 100000 and $nums2 <= 999999) {
+    for ($num = $nums1; $num <= $nums2; $num++) {
+        luckyNums($num);
+    }
+} else {
+    echo "Входные числа должны быть шестизначными!";
 }
 ?>
