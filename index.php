55c80d8ed00de059c07af87339e41f7305fc9b3d
Egor Shevchuk <egorshevchuk207@gmail.com>
2026-03-24 20:58:32 +0300

    v2

--- index.php
+++ index.php
@@ -1,13 +1,38 @@
 <?php
-$year = $_POST['year'];
+$year = $_POST['digit'];
 
-if ($year <= 30000) {
-    if ($year % 4 == 0 or $year % 400 ==0) {
-        echo "YES";
-    } else {
-        echo "NO";
-    } 
-}else {
-    echo "год больше 30000";
+switch ($year) {
+    case 0:
+        echo "Ноль";
+        break;
+    case 1:
+        echo "Один";
+        break;
+    case 2:
+        echo "Два";
+        break;
+    case 3:
+        echo "Три";
+        break;
+    case 4:
+        echo "Четыре";
+        break;
+    case 5:
+        echo "Пять";
+        break;
+    case 6:
+        echo "Шесть";
+        break;
+    case 7:
+        echo "Семь";
+        break;
+    case 8:
+        echo "Восемь";
+        break;
+    case 9:
+        echo "Девять";
+        break;
+    default:
+        echo "не входит в промежуток от 0 до 9";
 }
 ?>
