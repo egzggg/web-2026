6504e2e4313942d79853c3cdc17f6c734cba6312
Egor Shevchuk <egorshevchuk207@gmail.com>
2026-03-24 23:23:52 +0300

    v2

--- index.php
+++ index.php
@@ -1,24 +1,15 @@
 <?php
-$nums1 = $_POST['nums1'];
-$nums2 = $_POST['nums2'];
+$nums = $_POST['nums'];
 
-function sumNum(int $digit) {
-    return $digit % 100 + ((int)($digit / 10) % 10) + ($digit % 10);
-}
 
-function luckyNums(int $digit) {
-    $firstPart = sumNum((int)($digit / 1000));
-    $lastPart  = sumNum($digit % 1000);
-    if ($firstPart == $lastPart) {
-        echo $digit . "<br>";
+function factorial(int $num) {
+    if ($num <= 1) {          
+        return 1;
+    } else {
+        return $num * factorial($num - 1);  
     }
 }
 
-if ($nums1 >= 100000 and $nums1 <= 999999 and $nums2 >= 100000 and $nums2 <= 999999) {
-    for ($num = $nums1; $num <= $nums2; $num++) {
-        luckyNums($num);
-    }
-} else {
-    echo "Входные числа должны быть шестизначными!";
-}
+echo factorial($nums)
+
 ?>
