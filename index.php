0640fa8109266bd4334b45d750192687589b2b7e
Egor Shevchuk <egorshevchuk207@gmail.com>
2026-04-06 21:16:39 +0300

    v2

--- index.php
+++ index.php
@@ -1,34 +1,27 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <div>
-        <?php
-        $posts = [
-            [
-                'title' => 'The Road Ahead',
-                'subtitle' => '',
-                'img_modifier' => '',
-                'author' => '',
-                // другие свойства этого поста
-            ],
-            [
-                // свойства второго поста
-            ],
-        ];
-        ?>
-    </div>
-    <div>
-        <?php 
-        foreach ($posts as $post) {
-        include 'post_preview.php';
-        }
-        ?>
-    </div>
-    
-</body>
-</html>
+<?php
+$posts = [
+ [
+   'name' => 'Ваня Денисов',
+   'image' => '',
+   'image-icon' => '',
+   'likes' => '',
+   'comment-text' => ''
+   // другие свойства этого поста
+ ],
+ [
+   // свойства второго поста
+   'name' => 'Ваня Денисов',
+   'image' => '',
+   'image-icon' => '',
+   'likes' => '',
+   'comment-text' => ''
+ ],
+];
+?>
+                                    
+<?php 
+foreach ($posts as $post) {
+  include 'post_preview.php';
+}
+?>
+
--- post_preview.php
+++ post_preview.php
@@ -1,5 +1,14 @@
-<div>
-   <h3><?= $post['title'] ?></h3>
-   <h4><?= $post['subtitle'] ?></h4>
-   <span><?= $post['author'] ?></span>
+<div class="user-feed__post">
+    <div class="user-feed__icon">
+        <img class="user-feed__icon-image" src="images/larch.png" alt="yaric">
+        <h1 class="user-feed__name">Ваня Денисов</h1>
+        <img class="user-feed__icon-edit" src="images/pencil.png" alt="pensil">
+    </div>
+    <img class="user-feed__image" src="images/winter.png" alt="winter">
+    <div class="user-feed__comment">
+        <button class="user-feed__comment-button">❤️ 203</button>
+        <p class="user-feed__comment-massage">Так красиво сегодня на улице! Настоящая зима Вспоминается Бродский: «Поздно ночью, в уснувшей долине, на самом дне, в городк</p>
+        <p class="user-feed__comment-more-info">еще</p>
+        <p class="user-feed__comment-time">2 часа назад</p>
+    </div>
 </div>
