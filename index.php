f1b15ce3a96fb3ef09c82c4d762e6f3328763a0b
Egor Shevchuk <egorshevchuk207@gmail.com>
2026-04-06 22:57:41 +0300

    v4

--- index.php
+++ index.php
@@ -20,6 +20,16 @@ $posts = [
    'comment-more-info' => '&nbsp',
    'comment-time' => '1 день назад'
  ],
+ [
+   'name' => 'Ваня Денисов',
+   'image' => 'images/puncacke.png',
+   'image-icon' => 'images/larch.png',
+   'likes' => '❤️ 203',
+   'comment-text' => 'Так красиво сегодня на улице! Настоящая зима Вспоминается Бродский: «Поздно ночью, в уснувшей долине, на самом дне, в городк',
+   'comment-more-info' => 'еще',
+   'comment-time' => '2 часа назад'
+   // другие свойства этого поста
+ ],
 ];
 ?>                                         
 <?php 
