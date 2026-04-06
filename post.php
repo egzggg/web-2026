2f3f5254d7b127ae21aa720e3948578e49f2b3b9
Egor Shevchuk <egorshevchuk207@gmail.com>
2026-04-07 02:29:34 +0300

    v2

--- post.php
+++ post.php
@@ -11,7 +11,6 @@ include 'storage.php';
 
 $postId = $_GET['id'];
 
-$foundPost = null;
 
 foreach ($posts as $post) {
     if ($post['id'] == $postId) {
@@ -21,16 +20,14 @@ foreach ($posts as $post) {
 }
 ?>
 
-<?php if ($foundPost): ?>
-    <div>
-        <h3><?= $foundPost['id'] ?></h3>
-        <h3><?= $foundPost['name'] ?></h3>
-        <p><?= $foundPost['comment-text'] ?></p>
-        <span><?= $foundPost['comment-time'] ?></span>
-    </div>
-<?php else: ?>
-    <p>Пост не найден</p>
-<?php endif; ?>
+
+<div>
+    <h3><?= $foundPost['id'] ?></h3>
+    <h3><?= $foundPost['name'] ?></h3>
+    <p><?= $foundPost['comment-text'] ?></p>
+    <span><?= $foundPost['comment-time'] ?></span>
+</div>
+
 
 </body>
 </html>
