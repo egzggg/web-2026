78522d856ef50ff95364f2d6fbaf9fc4556a5438
Egor Shevchuk <egorshevchuk207@gmail.com>
2026-04-14 15:36:18 +0300

    added switch images v2

--- post_preview.php
+++ post_preview.php
@@ -7,9 +7,16 @@
     <div id="post-<?= $postIndex ?>" class="user-feed__image-container">
         
        <?php
+        $user_post_id = $_POST['post_id'] ?? 0;
+        $user_index = $_POST['index'] ?? 0;
+        if (isset($user_post_id) && (int)$user_post_id == $postIndex) {
+            $index = (int)($user_index ?? 0);
+        }
 
-       include 'switch_images.php' 
-       ?>
+        if ($index >= count($post['image'])) {
+            $index = 0;
+        }
+?>
 
         <?php if (count($post['image']) > 1){?>
             <form method="POST" action="#post-<?= $postIndex ?>">
